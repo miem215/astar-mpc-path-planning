13dd55e2236e4889dfd4d1ac5152e16c295450a9
Miya Su <msu@corp.e2mtechnologies.eu>
2026-05-10 16:16:23 +0200

    move the pictures into ./figure folder

--- visualizer.py
+++ visualizer.py
@@ -11,7 +11,7 @@ def save_plots(gmap, explored, astar_path, trajectory, obs):
         ax1.add_patch(plt.Rectangle((c-.45, r-.45), .9, .9, color='#B5D4F4', alpha=0.6))
     ax1.plot([p[1] for p in astar_path], [p[0] for p in astar_path], color='#378ADD', lw=2.5)
     ax1.set_title("Layer 1: A* Global Plan")
-    plt.savefig("01_astar.png")
+    plt.savefig("./figure/01_astar.png")
     plt.close()
 
     # 02_mpc_execution.png
@@ -23,7 +23,7 @@ def save_plots(gmap, explored, astar_path, trajectory, obs):
     ax2.add_patch(plt.Circle((obs.c, obs.r), 2.5, color='#F0997B', fill=False, ls='--')) # Safety Radius
     plt.legend()
     ax2.set_title("Layer 2: MPC Local Tracking")
-    plt.savefig("02_mpc_execution.png")
+    plt.savefig("./figure/02_mpc_execution.png")
     plt.close()
 
 def _draw_base(ax, gmap):
